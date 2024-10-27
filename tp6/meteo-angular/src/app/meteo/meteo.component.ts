35e82a1675d4c57feb10abb28e7cc444427fb8b3
Ninjacat-ctrl <mohamedlamine.nouri@edu.univ-paris13.fr>
2024-10-27 19:49:37 +0100

    last

--- tp6/meteo-angular/src/app/meteo/meteo.component.ts
+++ tp6/meteo-angular/src/app/meteo/meteo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { MeteoService } from "../services/meteo.service";
 import { MeteoItem } from "../meteoItem";
 
 @Component({
@@ -15,14 +16,37 @@ export class MeteoComponent implements OnInit {
 
   cityList: MeteoItem[] = [];
 
-  constructor() {}
+  constructor(private meteoService: MeteoService) { }
 
   ngOnInit() {
+    // const storedList = localStorage.getItem("cityList");
+
+    // if (storedList !== undefined && storedList !== null) {
+    //   this.cityList = JSON.parse(storedList);
+    // } else {
+    //   this.cityList = [];
+    //   this.cityList.forEach((city) => {
+    //     if (city.name) {
+    //       this.updateCityWeather(city);
+    //     }
+    //   });
+    // }
+
+
+    // Charger les données depuis le localStorage
     const storedList = localStorage.getItem("cityList");
 
     if (storedList !== undefined && storedList !== null) {
       this.cityList = JSON.parse(storedList);
+
+      // Après avoir chargé les données, mettre à jour la météo de chaque ville
+      this.cityList.forEach((city) => {
+        if (city.name) {
+          this.updateCityWeather(city);
+        }
+      });
     } else {
+      // Si aucune donnée n'est trouvée dans le localStorage, initialiser cityList comme un tableau vide
       this.cityList = [];
     }
   }
@@ -35,7 +59,7 @@ export class MeteoComponent implements OnInit {
       let currentCity = new MeteoItem();
       currentCity.name = this.city.name;
       this.cityList.push(currentCity);
-
+      this.fetchWeatherForCity(currentCity);
       this.saveCityList();
 
       console.log(this.city.name, "ajouté à la dans la liste");
@@ -52,6 +76,21 @@ export class MeteoComponent implements OnInit {
     this.saveCityList();
   }
 
+  updateCityWeather(city: MeteoItem): void {
+    if (city.name) {
+      this.meteoService.getMeteo(city.name).then(response => {
+        city.weather = {
+          icon: response.weather[0].id,
+          temp: response.main.temp
+        };
+        // Après mise à jour de la météo, sauvegarder dans le localStorage
+        this.saveCityList();
+      }).catch(error => {
+        console.error('Erreur lors de la récupération des données météo pour', city.name, error);
+      });
+    }
+  }
+
   isCityExist(_cityName: string) {
     // la méthode 'filter' retourne une liste contenant tous les items ayant un nom égale à _cityName
     // doc. sur filter : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/filter
@@ -66,7 +105,23 @@ export class MeteoComponent implements OnInit {
     }
   }
 
-  saveCityList() {
+  saveCityList(): void {
     localStorage.setItem("cityList", JSON.stringify(this.cityList));
   }
+
+  fetchWeatherForCity(city: MeteoItem) {
+    if (city.name) {
+      this.meteoService.getMeteo(city.name).then((response) => {
+        city.weather = {
+          temp: response.main.temp,
+          icon: response.weather[0].id,
+        };
+      }).catch((error) => {
+        console.error('Erreur lors de la récupération des données météo pour ', city.name, ':', error);
+      });
+    } else {
+      console.warn('Nom de la ville est indéfini');
+    }
+  }
+
 }
